47c62479ccda3019577f40e8104915733e23bd92
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-02 23:52:01 -0500

    puntos 6 y 5

--- Trabajo1.py
+++
@@ -1,6 +0,0 @@
-print("Programa para calcular el area de un triangulo")
-base=int(input("Digite la base: "))
-altura=int(input("Digite la altura: "))
-area=int((base*altura)/2)
-print("el area del triangulo es: ", area)
-exit()
