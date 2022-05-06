bfbd676f8b271dda963227ff546bcb3bee1e3a31
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-05 23:23:48 -0500

    punto8

---
+++ punto8.py
@@ -0,0 +1,8 @@
+
+print("Programa promedio")
+numero1= float(input("Digite el numero 1: "))
+numero2= float(input("Digite el numero 2: "))
+numero3= float(input("Digite el numero 3: "))
+promedio= float((numero1+numero2+numero3)/3)
+print("El promedio de los 3 numeros es: ","{:.5f}".format(promedio))
+exit()
