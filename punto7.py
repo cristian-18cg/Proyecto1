d24adfbf650067d250d6e4daedf996d4c701c905
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-05 23:11:48 -0500

    punto7

---
+++ punto7.py
@@ -0,0 +1,7 @@
+from math import pi
+print("Programa Circunferencia")
+radio=float(input("Digite el radio de la circunferencia: "))
+longitud=float(2*pi*radio)
+area=float(pi*(radio**2))
+print("El area del circulo es: ","{:.5f}".format(area),"\nLa longitud del circulo es:", "{:.5f}".format(longitud))
+exit()
