735cf5aeddb44dcfc7d0356505fe4d5c548a772f
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-10 22:08:40 -0500

    avance punto 3.6

---
+++ punto3,6.py
@@ -0,0 +1,11 @@
+import os
+
+num1=float(input("Digite numero 1: "))
+num2=float(input("Digite numero 2: "))
+if num1>num2:
+ print("El primer numero debe ser mayor al segundo")
+ os.wa
+ os.system ("cls")
+ 
+else:
+    print("Hola")
