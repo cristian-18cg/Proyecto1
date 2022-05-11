83df54dcbf87596a931739bc20f036ac98f3ac20
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-10 19:53:59 -0500

    punto 2.6 finalizado

---
+++ punto2,6.py
@@ -0,0 +1,8 @@
+a=float(input("Digite A: "))
+b=float(input("Digite B: "))
+if a<0 or b<0:
+    suma=float(a+b)
+    print("El resultado de a:",a," + b:",b ,"es:",suma)
+else:
+    mult=float(a*b)
+    print("El resultado de a:",a," * b:",b ,"es:",mult)
