ab04ad2d5b89d33cfee95ffffb1d50cf0909d5a5
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-10 19:04:47 -0500

    puntos 2 al 2.4 finalizados

---
+++ punto2,4.py
@@ -0,0 +1,8 @@
+A=float(input("Digite A: "))
+B=float(input("Digite B: "))
+if A<B:
+    suma=float(A+B)
+    print("la suma de A:",A," + ","B:",B," es:",suma)
+else:
+    resta=float(A-B)
+    print("la resta de A:",A," - ","B:",B," es:",resta)
---
+++ punto2,5.py
@@ -0,0 +1,8 @@
+def inicio():
+    inicio()
+ print("Programa para encontrar el cociente entre A y B")
+ A=float(input("Digite A: "))
+ B=float(input("Digite B: "))
+ if B==0:
+    print("La definicion no es posible")
+    inicio()
