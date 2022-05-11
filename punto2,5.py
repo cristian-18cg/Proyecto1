b92a473150e32894a7db1b5e8e109fd5ff718b11
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-10 19:25:43 -0500

    punto 2.5 finalizado

--- punto2,5.py
+++ punto2,5.py
@@ -1,8 +1,9 @@
-def inicio():
-    inicio()
- print("Programa para encontrar el cociente entre A y B")
- A=float(input("Digite A: "))
- B=float(input("Digite B: "))
- if B==0:
-    print("La definicion no es posible")
-    inicio()
+
+print("Programa para encontrar el cociente entre A y B")
+A=float(input("Digite A: "))
+B=float(input("Digite B: "))
+if B==0:
+ print("La definicion no es posible")
+else:
+ cociente=float(A/B)
+ print("El cociente de A: ",A," entre B: ",B," Es: ",cociente)
