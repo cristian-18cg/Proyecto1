ce375c2800d22fabf4946fecdda727f6ce7c4430
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-12 22:35:59 -0500

    punto 3.6 finalizado

--- punto3,6.py
+++ punto3,6.py
@@ -1,11 +1,18 @@
 import os
+import time
+import platform
 
-num1=float(input("Digite numero 1: "))
-num2=float(input("Digite numero 2: "))
-if num1>num2:
- print("El primer numero debe ser mayor al segundo")
- os.wa
- os.system ("cls")
- 
-else:
-    print("Hola")
+def inicio():
+ num1=int(input("Digite numero 1: "))
+ num2=int(input("Digite numero 2: "))
+ if num1>num2:
+  print("El primer numero debe ser mayor al segundo")
+  time.sleep(2)
+  os.system('cls')
+  return inicio()
+ else:
+   while num2>=num1:
+       print (num1)
+       num1=num1+1
+
+inicio()
