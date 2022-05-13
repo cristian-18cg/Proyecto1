47aa189104e382bd1d5d6c96689a4458bb9140af
cristian-18cg <cfgarcia654@misena.edu.co>
2022-05-12 23:53:51 -0500

    menus inicio

--- Miscelanea_ejercicios.py
+++ Miscelanea_ejercicios.py
@@ -3,8 +3,36 @@ import os
 import time
 import platform
 def inicio():
+ def operadores():
+  os.system('cls')
+  print("Operadores\n  1. Calcular el área de un triángulo.\n  2.Ingresar dos números por teclado y sumarlos.\n  3. Ingresar un número y visualizar el número elevado al cuadrado\n  4. Escribir un algoritmo que convierta de euros a dólares.\n 5. Escribir un algoritmo que pida el lado de un cuadrado y muestre el valor del área y del perímtro. ")
+ operadores()
+
+ def condicionales():
+  os.system('cls')
+  print("hola")
+ condicionales()
+
+ def ciclos():
+  os.system('cls')
+  print("hola")
+ ciclos()
+
+
+ os.system('cls')
  print ("Miscelanea ejercicios python")
- print ()
- while true:
-     
-    
+ menu1=int(input("1. Operadores \n2. Condicionales \n3. Ciclos\n99. Salir \nDigite una opcion:"))
+ if menu1==1:
+    return operadores()
+ elif menu1==2:
+    return condicionales()
+ elif menu1==3:
+    return ciclos()
+ elif menu1==99:
+     print("Fin del programa")
+     exit()  
+ else:
+     print("Digite una opcion valida")
+     time.sleep(2)
+     return inicio()      
+inicio()
