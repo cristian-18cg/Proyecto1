8574c3d58166f98e9ad2f5e40f0481f2543f9a86
cristian-18cg <cfgarcia654@misena.edu.co>
2022-06-08 22:04:33 -0500

    Reduccion de codigo menu 1

--- Miscelanea_ejercicios.py
+++ Miscelanea_ejercicios.py
@@ -6,12 +6,12 @@ from tkinter import E
 def inicio():
  os.system('cls')
  print ("Miscelanea ejercicios python")
- menu1=int(input("1. Operadores \n2. Condicionales \n3. Ciclos\n99. Salir \nDigite una opcion:"))
- if menu1==1:
+ menu1=input("1. Operadores \n2. Condicionales \n3. Ciclos\n99. Salir \nDigite una opcion:")
+ if menu1=='1':
   def menu_2(): 
     os.system('cls')
-    menu2=int(input("Operadores\n  1. Area de un triángulo.\n  2. Ingresar dos números por teclado y sumarlos.\n  3. Cuadrado de un numero\n  4. Conversor Euro a Dolar \n  5. Area y perímetro de un cuadrado. \n  6. Area y el volúmen de un cilindro. \n  7. Circurferencia. \n  8. Promedio de tres (3) números ingresados por teclado.\n  9. Volver al menu inicio \n  99. Salir \nDigite una opcion:"))
-    if menu2==1:
+    menu2=input("Operadores\n  1. Area de un triángulo.\n  2. Ingresar dos números por teclado y sumarlos.\n  3. Cuadrado de un numero\n  4. Conversor Euro a Dolar \n  5. Area y perímetro de un cuadrado. \n  6. Area y el volúmen de un cilindro. \n  7. Circurferencia. \n  8. Promedio de tres (3) números ingresados por teclado.\n  9. Volver al menu inicio \n  99. Salir \nDigite una opcion:")
+    if menu2=='1':
       os.system('cls')
       print("Programa para calcular el area de un triangulo")
       base=int(input("Digite la base: "))
@@ -20,7 +20,7 @@ def inicio():
       print("el area del triangulo es: ", area)
       time.sleep(3)
       return menu_2() 
-    elif menu2==2:
+    elif menu2=='2':
       os.system('cls')
       print("Programa para sumar dos numeros")
       numero1=int(input("Digite numero 1: "))
@@ -29,7 +29,7 @@ def inicio():
       print("La suma de los dos numeros es: ",suma)
       time.sleep(3)
       return menu_2()
-    elif menu2==3:
+    elif menu2=='3':
       os.system('cls')
       print("Programa para mostrar el cuadrado de un numero ")
       numero1=float(input("Digite el numero: "))
@@ -38,7 +38,7 @@ def inicio():
       print("La potencia del numero es: ",resultado)
       time.sleep(3)
       return menu_2() 
-    elif menu2==4:
+    elif menu2=='4':
       os.system('cls')
       print("Programa conversor euros a dolares")
       numero1=float(input("Digite los euros: "))
@@ -47,7 +47,7 @@ def inicio():
       print("Son: ",conversor ,"USD")
       time.sleep(3)
       return menu_2()
-    elif menu2==5:
+    elif menu2=='5':
       os.system('cls')
       print("Programa cuadrado")
       numero1=float(input("Digite uno de los lados del cuadrado: "))
@@ -58,7 +58,7 @@ def inicio():
       print("El area del cuadrado es: ",area ,"\nEl perimetro del cuadrado es: ", perimetro)
       time.sleep(3)
       return menu_2() 
-    elif menu2==6:
+    elif menu2=='6':
       os.system('cls')
       print("Programa para halla area y volumen de un cilindro")
       altura=float(input("Digite la altura del cilindro: "))
@@ -68,16 +68,16 @@ def inicio():
       print("El area del cilindro es: ",area ,"\nEl volumen del cilindro es:", volumen)
       time.sleep(3)
       return menu_2() 
-    elif menu2==7:
+    elif menu2=='7':
       os.system('cls')
       print("Programa Circunferencia")
       radio= float(input("Digite el radio de la circunferencia: "))
-      longitud= float(2*pi*radio)
+      longitud=float(2*pi*radio)
       area= float(pi*(radio**2))
       print("El area del circulo es: ","{:.5f}".format(area),"\nLa longitud del circulo es:", "{:.5f}".format(longitud))
       time.sleep(3)
       return menu_2()
-    elif menu2==8:
+    elif menu2=='8':
       os.system('cls')
       print("Programa promedio")
       numero1=float(input("Digite el numero 1: "))
@@ -87,9 +87,8 @@ def inicio():
       print("El promedio de los 3 numeros es: ","{:.5f}".format(promedio))
       time.sleep(3)
       return menu_2()
-    elif menu2==9:
-      return inicio()
-    elif menu2==99:
+    elif menu2=='9':return inicio()     
+    elif menu2=='99':
       os.system('cls')
       print("Fin del programa...")
       time.sleep(2)
@@ -100,45 +99,32 @@ def inicio():
        time.sleep(2)
        return menu_2()
   menu_2()
- elif menu1==2:
+ elif menu1=='2':
     def menu_condicionales(): 
      os.system('cls')
-     menu3=int(input("Condicionales\n  1. Numero positivo o negativo.\n  2. Cual numero es mayor y cual menor.\n  3. Mayor y menor de 3 numeros\n  4. Dados dos números A y B, sumarlos si A es menor que B o sino restarlos. \n  5. Dados dos números A y B encontrar el cociente entre A y B. \n  6. A y B sumarlos si al menos uno de ellos es negativo, en caso contrario multiplicarlos.  \n  7. Año biciesto. \n  8. Volver al menu inicio \n  99. Salir \nDigite una opcion:"))
-     if menu3== 1:
+     menu3=input("Condicionales\n  1. Numero positivo o negativo.\n  2. Cual numero es mayor y cual menor.\n  3. Mayor y menor de 3 numeros\n  4. Dados dos números A y B, sumarlos si A es menor que B o sino restarlos. \n  5. Dados dos números A y B encontrar el cociente entre A y B. \n  6. A y B sumarlos si al menos uno de ellos es negativo, en caso contrario multiplicarlos.  \n  7. Año biciesto. \n  8. Volver al menu inicio \n  99. Salir \nDigite una opcion:")
+     if menu3=='1':
         os.system('cls')
         print("Programa para evaluar signo del numero")
-        numero1=0
         numero1=float(input("Digite el numero: "))
-        if numero1 >= 1:
-            print("el numero es positivo")
-        elif numero1 == 0:
-            print("el numero es neutro")
-        else:
-            print("el numero es negativo")
+        if numero1 >= 1:print("el numero es positivo")
+        elif numero1 == 0:print("el numero es neutro")
+        else:print("el numero es negativo")
         time.sleep(2)
         return menu_condicionales()
-     elif menu3==2:
+     elif menu3=='2':
        os.system('cls') 
        print("Programa para comparar dos numeros")
-       numero1=0 
-       numero2=0
        numero1=float(input("Digite el numero 1: "))
        numero2=float(input("Digite el numero 2: "))
-       if numero1 > numero2:
-           print("El numero ",numero1, "es mayor","\nEl numero ",numero2, "es menor")
-       elif numero1 == numero2:
-           print("El numero 1: ",numero1, " y el numero 2: ",numero2, "son iguales")
-       else:
-           print("El numero ",numero2, "es mayor","\nEl numero ",numero1, "es menor" )  
+       if numero1 > numero2:print("El numero ",numero1, "es mayor","\nEl numero ",numero2, "es menor")
+       elif numero1 == numero2:print("El numero 1: ",numero1, " y el numero 2: ",numero2, "son iguales")
+       else:print("El numero ",numero2, "es mayor","\nEl numero ",numero1, "es menor" )  
        time.sleep(2)
        return menu_condicionales()     
-     elif menu3==3:
+     elif menu3=='3':
          os.system('cls')
          print("Programa para comparar tres numeros")
-         numero1=0 
-         numero2=0
-         numero3=0
-         mayor=0
          numero1=float(input("Digite el numero 1: "))
          numero2=float(input("Digite el numero 2: "))
          numero3=float(input("Digite el numero 3: "))
@@ -165,16 +151,14 @@ def inicio():
               print("los numeros son iguales")
          time.sleep(2)
          return menu_condicionales()
-     elif menu3==4:
+     elif menu3=='4':
         os.system('cls')
         A=float(input("Digite A: "))
         B=float(input("Digite B: "))
         if A<B:
-         suma=float(A+B)
-         print("la suma de A:",A," + ","B:",B," es:",suma)
+         print("la suma de A:",A," + ","B:",B," es:",A+B)
         else:
-         resta=float(A-B)
-         print("la resta de A:",A," - ","B:",B," es:",resta)
+         print("la resta de A:",A," - ","B:",B," es:",A-B)
         time.sleep(2)
         return menu_condicionales()
      elif menu3==5:
@@ -234,7 +218,7 @@ def inicio():
        time.sleep(2)
        return menu_condicionales()
     menu_condicionales()
- elif menu1==3:
+ elif menu1=='3':
     def menu_ciclos(): 
      os.system('cls')
      menu4=int(input("Ciclos\n  1. Múltiplos de 3 que hay entre 1 y 100.\n  2. Números impares entre 0 y 100.\n  3. Números pares del 1 al 100.\n  4. Cuadrados de los números del 1 al 30. \n  5. Suma los cuadrados de los cien primeros números naturales. \n  6. Números comprendidos entre ellos en secuencia ascendente.  \n  7. Sumar todos los números que se digitan por teclado mientras no sea cero. \n  8. Volver al menu inicio \n  99. Salir \nDigite una opcion:"))
@@ -325,7 +309,7 @@ def inicio():
         return menu_ciclos()           
 
     menu_ciclos()    
- elif menu1==99:
+ elif menu1=='99':
      os.system('cls')
      print("Fin del programa...")
      time.sleep(2)
